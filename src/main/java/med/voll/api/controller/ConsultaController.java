d28a26957e9e755bc9eafc2629ad3acf65bd74c9
Lucas Vilhena <66475011+Fulanalok@users.noreply.github.com>
2024-11-12 20:20:54 -0300

    Add files via upload

---
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -0,0 +1,27 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.domain.consulta.AgendaDeConsultas;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.domain.consulta.DadosDetalhamentoConsulta;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("consultas")
+public class ConsultaController {
+
+    private final AgendaDeConsultas agenda;
+
+    public ConsultaController(AgendaDeConsultas agenda) {
+        this.agenda = agenda;
+    }
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity<DadosDetalhamentoConsulta> agendar(@RequestBody @Valid DadosAgendamentoConsulta dados) {
+        var dto = agenda.agendar(dados);
+        return ResponseEntity.ok(dto);
+    }
+}
